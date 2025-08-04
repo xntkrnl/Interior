530c0a7947ec5203f8aca7220dabeec02f6f66d2
xntkrnl <maminbors@gmail.com>
2025-08-04 20:38:42 +0300

    OnStartRandom (for lamps)

---
+++ Scripts/OnStartRandom.cs
@@ -0,0 +1,23 @@
+﻿using Interior.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interior.Scripts
+{
+    internal class OnStartRandom : MonoBehaviour
+    {
+        public UnityEvent? onStart;
+        [Range(0f, 100f)]
+        public float onStartChance;
+
+        void Start()
+        {
+             if (Randomizer.Instance.rnd.Next(100) + 1 <= onStartChance)
+                onStart.Invoke();
+        }
+    }
+}
